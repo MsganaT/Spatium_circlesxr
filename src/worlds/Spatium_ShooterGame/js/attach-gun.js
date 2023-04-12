ab79270f6c2233b5726d27e30d36dc99b0369a2b
MsganaT <101358591+MsganaT@users.noreply.github.com>
2023-04-11 22:58:50 -0400

    Create attach-gun.js
    Add attach gun to player component

---
+++ src/worlds/Spatium_ShooterGame/js/attach-gun.js
@@ -0,0 +1,26 @@
+AFRAME.registerComponent('attach-gun', {
+    init: function() {
+        
+        CIRCLES.getAvatarRigElement().setAttribute('movement-controls', {enabled:false});
+        //console.log("movement locked");
+        CIRCLES.getMainCameraElement().setAttribute('shooter','');
+        CIRCLES.getMainCameraElement().setAttribute('click-to-shoot', '');
+        var cursorAttachment = document.querySelector('#cursor');
+        CIRCLES.getMainCameraElement().appendChild(cursorAttachment);
+        
+            CIRCLES.getCirclesSceneElement().addEventListener(CIRCLES.EVENTS.READY, function() {
+                //tried to include code here but it would not run 
+
+                /** 
+                 
+                CIRCLES.getAvatarRigElement().setAttribute('movement-controls', {enabled:false});
+                console.log("movement locked");
+                CIRCLES.getMainCameraElement().setAttribute('shooter','');
+                CIRCLES.getMainCameraElement().setAttribute('click-to-shoot', '');
+
+                */
+        
+            });
+    }
+
+});
